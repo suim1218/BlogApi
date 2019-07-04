bae876b2e8a17eb1be1a27ec3fab27b934c80f66
suim1218 <30396887+suim1218@users.noreply.github.com>
2019-07-04 09:55:24 +0800

    Update views.py

--- article/views.py
+++ article/views.py
@@ -56,7 +56,7 @@ def add_article(request):
                     "author": author,
                     "content": content
                 }
-                return JsonResponse({'status': 1, 'data': data, 'message': 'add article successaaa'})
+                return JsonResponse({'status': 1, 'data': data, 'message': 'add article success'})
     else:
         return JsonResponse({'status': 0, 'message': 'request type error'})
 
